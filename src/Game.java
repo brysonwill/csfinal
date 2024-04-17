0509e8d3a61b17044da0544b8a1d7b4ed902e5b5
S1967419 <S1967419@S0085CG9172N67.student.HISD.ORG>
2024-04-17 09:17:48 -0500

    added background
    Signed-off-by: S1967419 <S1967419@S0085CG9172N67.student.HISD.ORG>

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,170 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
 
-public class Game {
-
+public class Game extends JPanel implements Runnable,KeyListener{
+	private BufferedImage back;
+	private pictures bi;
+	private pictures p ;
+	private boolean startgame;
+	private ImageIcon Background ;
+	
+	
+	
+	
+	
+	public Game() {
+		back=null;
+	new Thread (this).start();
+		Background = new ImageIcon("Background.jpg");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
+	
+	
