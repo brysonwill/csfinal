a96b9320dc60b02777edc22869f6a1b115b691f1
S1967419 <S1967419@S0085CG9172N67.student.HISD.ORG>
2024-04-19 09:20:27 -0500

    game updates

--- src/Game.java
+++ src/Game.java
@@ -3,168 +3,113 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
 
-public class Game extends JPanel implements Runnable,KeyListener{
-	private BufferedImage back;
-	private pictures bi;
-	private pictures p ;
-	private boolean startgame;
-	private ImageIcon Background ;
-	
-	
-	
-	
-	
-	public Game() {
-		back=null;
-	new Thread (this).start();
-		Background = new ImageIcon("Background.jpg");
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
-	
+public class Game extends JPanel implements Runnable, KeyListener, MouseListener, MouseMotionListener {
+    private BufferedImage back;
+    private ImageIcon Background;
+    private boolean startgame;
+    private boolean start, win, moveRight;
+    private char screen;
+
+    public Game() {
+        back = null;
+        new Thread(this).start();
+        Background = new ImageIcon("Background.jpg");
+        this.addKeyListener(this);
+        this.addMouseListener(this);
+        this.addMouseMotionListener(this);
+        start = false; 
+        screen = 'S';
+    }
+
+    public void screen(Graphics g2d) {
+        switch (screen) {
+            case 'S':
+                g2d.drawImage(Background.getImage(), 0, 0, getWidth(), getHeight(), this);
+                drawStartScreen(g2d);
+                break;
+        }
+    }
+
+    public void drawStartScreen(Graphics g2d) {
+        
+        g2d.setFont(new Font("Broadway", Font.BOLD, 35));
+        g2d.setColor(Color.white);
+        g2d.drawString("Welcome to zombie attack !", 50, 300);
+        g2d.drawString("Press SPACE to Begin", 200, 400);
+    }
+
+    @Override //chat gpt
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        screen(g);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // Start the game when SPACE key is pressed
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            start = true;
+            // Add code here to start the game logic or transition to the next screen
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
 }
-	
-	
